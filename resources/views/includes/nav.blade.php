0b255cdc89b92f92f801d02b02dc9e8c7f9d7514
stevenmvn <pro.morvan.steven@gmail.com>
2022-04-24 03:19:27 +0200

    Update GitHub link

--- resources/views/includes/nav.blade.php
+++ resources/views/includes/nav.blade.php
@@ -3,6 +3,6 @@
     <img class="nav__mobile" src="{{ asset('images/burger.svg') }}" alt="Icone menu mobile">
     <ul class="nav__menu">
         <li class="nav__item"><a href="{{ route('products.index') }}">Explorer nos NFT</a></li>
-        <li class="nav__item"><a href="#" class="btn btn__img btn--primary"><img src="{{ asset('images/github.svg') }}" alt="Icone github">Github du projet</a></li>
+        <li class="nav__item"><a href="https://github.com/stevenmvn/justmining" class="btn btn__img btn--primary"><img src="{{ asset('images/github.svg') }}" alt="Icone github">Github du projet</a></li>
     </ul>
 </nav>
